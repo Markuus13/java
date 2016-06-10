0b63d7e0e384c972336f97e91143e6741b4445ce
Marcus <marcus.f22@gmail.com>
2016-06-10 14:10:22 +0000

    Capitulo 5 iniciado - adicionadas classes necessárias

---
+++ FJ-11 Java e Orientação a Objetos/5 - Um pouco de Arrays/Funcionario.java
@@ -0,0 +1,24 @@
+class Funcionario {
+    String nome;
+    String departamento;
+    double salario;
+    Data dataDeEntrada;
+    String rg;
+    
+    // Métodos
+    void recebeAumento(double aumento) {
+        this.salario += aumento;
+    }
+    
+    double calculaGanhoAnual() {
+        return this.salario * 12;
+    }
+    
+    void mostra() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Departamento: " + this.departamento);
+        System.out.println("Salário: " + this.salario);
+        System.out.println("Data de entrada: " + this.dataDeEntrada.formatada());
+        System.out.println("RG: " + this.rg);
+    }
+}
