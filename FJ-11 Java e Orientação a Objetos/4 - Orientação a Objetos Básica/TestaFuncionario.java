361a0d4fcc26dbedad2f1450e5a17ad702921c7b
Marcus <marcus.f22@gmail.com>
2016-06-10 13:33:08 +0000

    Testando dataDeEntrada de Funcionario

--- FJ-11 Java e Orientação a Objetos/4 - Orientação a Objetos Básica/TestaFuncionario.java
+++ FJ-11 Java e Orientação a Objetos/4 - Orientação a Objetos Básica/TestaFuncionario.java
@@ -1,10 +1,18 @@
 class TestaFuncionario {
     public static void main(String[] args) {
         Funcionario f1 = new Funcionario();
+        Data data = new Data();
+        
+        data.dia = 13;
+        data.mes = 07;
+        data.ano = 2016;
         
         f1.nome = "Marcus";
         f1.salario = 500000;
         f1.recebeAumento(10);
+        
+        // Ligação
+        f1.dataDeEntrada = data;
         
         f1.mostra();
     }
