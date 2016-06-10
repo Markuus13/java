7f70774ce4d6186108fb9ab7a63962b7d9e9141a
Marcus <marcus.f22@gmail.com>
2016-06-10 12:27:33 +0000

    Capitulo 3 - Ex8 -> fors encadeados

---
+++ FJ-11 Java e Orientação a Objetos/3 - Variaveis Primitivas e Controle de Fluxo/Exercicio8.java
@@ -0,0 +1,11 @@
+class Exercicio8 {
+    public static void main (String[] args) {
+        
+        for (int n=1; n<=4; n++){
+            for (int m=1; m<=n; m++){
+                System.out.print(m*n+" ");
+            }
+            System.out.println();
+        }
+    }
+}
