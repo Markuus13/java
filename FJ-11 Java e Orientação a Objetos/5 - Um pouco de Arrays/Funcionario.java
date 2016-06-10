c134d46d5cbba4e8b5822203da562ddd4984b4b1
Marcus <marcus.f22@gmail.com>
2016-06-10 14:56:44 +0000

    Método adiciona(Funcionario f) da classe Empresa refatorado para aumentar a array dinamicamente conforme mais funcionários são adicionados

--- FJ-11 Java e Orientação a Objetos/5 - Um pouco de Arrays/Funcionario.java
+++ FJ-11 Java e Orientação a Objetos/5 - Um pouco de Arrays/Funcionario.java
@@ -37,8 +37,22 @@ class Empresa {
             indice++;
         }
        
-        else
-            System.out.println("Erro! Limite de funcionários alcançado.");
+        else {
+            // Aloca uma nova rray com mais 10 espaços
+            Funcionario[] aux = new Funcionario[indice + 10];
+            
+            // Copia os valores da antiga nas mesmas posições da nova
+            for (int i=0; i<indice; i++)
+                aux[i] = empregados[i];
+            
+            // O atributo empregados deste objeto passa apontar para a nova array
+            this.empregados = aux;
+            
+            // Faz uma chamada recursiva para adicionar o primeiro funcionario da nova array
+            this.adiciona(f);
+        
+        }
+        
     }
     
     void mostraEmpregados() {
--- FJ-11 Java e Orientação a Objetos/5 - Um pouco de Arrays/TestaEmpresa.java
+++ FJ-11 Java e Orientação a Objetos/5 - Um pouco de Arrays/TestaEmpresa.java
@@ -3,20 +3,21 @@ class TestaEmpresa {
         
         Empresa emp = new Empresa();
         emp.empregados = new Funcionario[10];
-        /*
-        for (int i=0; i<10; i++) {
+        
+        for (int i=0; i<40; i++) {
             Funcionario f = new Funcionario();
             f.salario = 1000 + i * 100;
             emp.adiciona(f);
         }
         
         emp.mostraEmpregados();
-        */
+        
+       /* 
         Funcionario f = new Funcionario();
         Funcionario f2 = new Funcionario();
         emp.adiciona(f);
         System.out.println("Contem funcionario f: " + emp.contem(f));
         System.out.println("Contem funcionario f2: " + emp.contem(f2));
-        
+        */
     }
 }
