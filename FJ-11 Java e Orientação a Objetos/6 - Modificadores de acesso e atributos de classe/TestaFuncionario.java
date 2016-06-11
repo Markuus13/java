a43a2322bfa0a9ef6e9f8501c37549c847713a7a
Marcus <marcus.f22@gmail.com>
2016-06-11 16:47:58 +0000

    Classe de Teste adicionada

---
+++ FJ-11 Java e Orientação a Objetos/6 - Modificadores de acesso e atributos de classe/TestaFuncionario.java
@@ -0,0 +1,10 @@
+class TestaFuncionario {
+
+    public static void main(String[] args) {
+        
+        Funcionario f = new Funcionario();
+        
+        f.setSalario(5000);
+        f.mostra();
+    }
+}
