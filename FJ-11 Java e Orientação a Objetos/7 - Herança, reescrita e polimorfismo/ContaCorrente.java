2782f2619361fa5bbaf845a1b59ea7faaa30dbc2
Marcus <marcus.f22@gmail.com>
2016-06-11 22:25:50 +0000

    Método reescrito atualiza(double taxa) refatorado

--- FJ-11 Java e Orientação a Objetos/7 - Herança, reescrita e polimorfismo/ContaCorrente.java
+++ FJ-11 Java e Orientação a Objetos/7 - Herança, reescrita e polimorfismo/ContaCorrente.java
@@ -2,7 +2,8 @@ public class ContaCorrente extends Conta {
     
     @Override
     public void atualiza(double taxa) {
-        this.saldo += this.saldo * taxa * 2;
+        super.atualiza(taxa);
+        this.saldo *= 2;
     }
     
     @Override
--- FJ-11 Java e Orientação a Objetos/7 - Herança, reescrita e polimorfismo/ContaPoupanca.java
+++ FJ-11 Java e Orientação a Objetos/7 - Herança, reescrita e polimorfismo/ContaPoupanca.java
@@ -2,6 +2,7 @@ public class ContaPoupanca extends Conta {
     
     @Override
     public void atualiza(double taxa) {
-        this.saldo += this.saldo * taxa * 3;
+        super.atualiza(taxa);
+        this.saldo *= 3;
     }
 }
