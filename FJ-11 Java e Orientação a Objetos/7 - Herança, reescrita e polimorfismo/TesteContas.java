4dd78793c6b8a1aa1f7d32a405748afcad1b973c
Marcus <marcus.f22@gmail.com>
2016-06-11 21:47:37 +0000

    Classe de TesteContas adicionada

---
+++ FJ-11 Java e Orientação a Objetos/7 - Herança, reescrita e polimorfismo/TesteContas.java
@@ -0,0 +1,19 @@
+public class TesteContas {
+    public static void main(String[] args) {
+        Conta c = new Conta();
+        ContaCorrente cc = new ContaCorrente();
+        ContaPoupanca cp = new ContaPoupanca();
+        
+        c.deposita(1000);
+        cc.deposita(1000);
+        cp.deposita(1000);
+        
+        c.atualiza(0.01);
+        cc.atualiza(0.01);
+        cp.atualiza(0.01);
+        
+        System.out.println("Conta: " + c.getSaldo());
+        System.out.println("Conta Corrente: " + cc.getSaldo());
+        System.out.println("Conta Poupanca: " + cp.getSaldo());
+    }
+}
