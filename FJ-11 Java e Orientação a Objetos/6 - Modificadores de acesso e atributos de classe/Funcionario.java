408376a02435c6e590717c0590544f0d5fbe193a
Marcus <marcus.f22@gmail.com>
2016-06-11 14:11:12 +0000

    Encapsulamento da classe Funcionario

--- FJ-11 Java e Orientação a Objetos/6 - Modificadores de acesso e atributos de classe/Funcionario.java
+++ FJ-11 Java e Orientação a Objetos/6 - Modificadores de acesso e atributos de classe/Funcionario.java
@@ -1,24 +1,66 @@
 class Funcionario {
-    String nome;
-    String departamento;
-    double salario;
-    Data dataDeEntrada;
-    String rg;
+    private String nome;
+    private String departamento;
+    private double salario;
+    private Data dataDeEntrada = new Data();
+    private String rg;
     
-    // Métodos
-    void recebeAumento(double aumento) {
+    /* Métodos */
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getDepartamento() {
+        return this.departamento;
+    }
+    
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+    
+    public double getSalario() {
+        return this.salario;
+    }
+    
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    public void recebeAumento(double aumento) {
         this.salario += aumento;
     }
     
-    double calculaGanhoAnual() {
+    public Data getData() {
+        return this.dataDeEntrada;
+    }
+    
+    public void setData(int dia, int mes, int ano) {
+        this.dataDeEntrada.dia = dia;
+        this.dataDeEntrada.mes = mes;
+        this.dataDeEntrada.ano = ano;
+    }
+    
+    public String getRG() {
+        return this.rg;
+    }
+    
+    public void setRG(String rg) {
+        this.rg = rg;
+    }
+    
+    public double getGanhoAnual() {
         return this.salario * 12;
     }
     
-    void mostra() {
+    public void mostra() {
         System.out.println("Nome: " + this.nome);
         System.out.println("Departamento: " + this.departamento);
         System.out.println("Salário: " + this.salario);
-     //   System.out.println("Data de entrada: " + this.dataDeEntrada.formatada());
+        System.out.println("Data de entrada: " + this.dataDeEntrada.formatada());
         System.out.println("RG: " + this.rg);
     }
 }
